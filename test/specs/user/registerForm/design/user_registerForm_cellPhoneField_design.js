3ca7c4bc6ce7b3e092177813da57a232d005bb86
TatsianaH <ourflsun@gmail.com>
2019-09-26 11:53:39 -0400

    changed names of tests

--- test/specs/user/registerForm/design/user_registerForm_cellPhoneField_design.js
+++ test/specs/user/registerForm/design/user_registerForm_cellPhoneField_design.js
@@ -13,14 +13,14 @@ describe('Cell Phone Number input field', () => {
         assert.isTrue(actualInputFieldPhone);
     });
 
-    it('should verify border-color of "Cell phone number" input field ', () => {
+    it('should check border-color of "Cell phone number" input field ', () => {
         const actualBorder = $(inputFieldPhone).getCSSProperty('border-top-color').parsed.hex;
         console.log('!!!!!!!!' + actualBorder);
         const expectedBorder = '#ced4da';
         assert.equal(actualBorder, expectedBorder);
     });
 
-    it('should verify font-color of "Cell phone number" input field when user enters first symbol', () => {
+    it('should check font-color of "Cell phone number" input field when user enters first symbol', () => {
         const input = $(inputFieldPhone).setValue('+');
         const actualFontColor = $(inputFieldPhone).getCSSProperty('color').parsed.hex;
         console.log('!!!!!!!!' + actualFontColor);
@@ -28,34 +28,34 @@ describe('Cell Phone Number input field', () => {
         assert.equal(actualFontColor, expectedFontColor);
     });
 
-    it('should verify focus border-color of "Cell phone number" input field', () => {
+    it('should check focus border-color of "Cell phone number" input field', () => {
         const actualFocusBorderColor = $(inputFieldPhone).getCSSProperty('border-color').parsed.hex;
         console.log('!!!!!!!!' + actualFocusBorderColor);
         const expectedFocusBorderColor = '#4d94ff';
         assert.equal(actualFocusBorderColor, expectedFocusBorderColor);
     });
 
-    it('should verify focus highlight color of "Cell phone number', () => {
+    it('should check focus highlight color of "Cell phone number', () => {
         const actualFocusHighlightColor = $(inputFieldPhone).getCSSProperty('box-shadow').parsed.hex;
         console.log('!!!!!!!!' + actualFocusHighlightColor);
         const expectedFocusHighlightColor = '#0052cc';
         assert.equal(actualFocusHighlightColor, expectedFocusHighlightColor);
     });
-    it('should verify background-color of "Cell phone number" input field when user enters first symbol', () => {
+    it('should check background-color of "Cell phone number" input field when user enters first symbol', () => {
         const actualBackgroundColor = $(inputFieldPhone).getCSSProperty('background-color').parsed.hex;
         console.log('!!!!!!!!' + actualBackgroundColor);
         const expectedBackgroundColor = '#ffffff';
         assert.equal(actualBackgroundColor, expectedBackgroundColor);
     });
 
-    it('should verify text-align of "Cell phone number" input field when user enters first symbol', () => {
+    it('should check text-align of "Cell phone number" input field when user enters first symbol', () => {
         const actualTextAlign = $(inputFieldPhone).getCSSProperty('text-align').value;
         console.log('!!!!!!!!' + actualTextAlign);
         const expectedTextAlign = 'start';
         assert.equal(actualTextAlign, expectedTextAlign);
     });
 
-    it('should verify font-family of "Cell phone number" input field when user enters first symbol.', () => {
+    it('should check font-family of "Cell phone number" input field when user enters first symbol.', () => {
         const fontFamilyObj = $(inputFieldPhone).getCSSProperty('font-family');
         const valuesfontFamilyObj = Object.values(fontFamilyObj)[2];
         const actualfontFamily = Object.values(valuesfontFamilyObj).join();
@@ -64,7 +64,7 @@ describe('Cell Phone Number input field', () => {
         assert.equal(actualfontFamily, expectedfontFamily);
     });
 
-    it('should verify font-weight of "Cell phone number" input field when user enters first symbol.', () => {
+    it('should check font-weight of "Cell phone number" input field when user enters first symbol.', () => {
         const actualWeightObj = $(inputFieldPhone).getCSSProperty('font-weight');
         const actualWeight = Object.values(actualWeightObj)[1];
         console.log('!!!!!!!!' + actualWeight);
@@ -80,11 +80,9 @@ describe('Cell Phone Number input field when the number is validated', () => {
         const input = $(inputFieldPhone).setValue('+17008009000');
         browser.keys('Tab');
         browser.pause(500);
-
     });
 
-    it('should verify border-color of "Cell phone number" input field when the number is validated.', () => {
-
+    it('should check border-color of "Cell phone number" input field when the number is validated.', () => {
         const actualBorderColorValid = $(inputFieldPhone).getCSSProperty('border-color').parsed.hex;
         console.log('!!!!!!!!' + actualBorderColorValid);
         const expectedBorderColorValid = '#24c88b';
@@ -97,12 +95,12 @@ describe('Cell phone number Label', () => {
         browser.url(url.registerUrl);
     });
 
-    it('should verify that label “Cell phone number”  is displayed.', () => {
+    it('should check that label “Cell phone number”  is displayed.', () => {
         const actualLabel = $('//label[@for="phone"]').isDisplayed();
         assert.isTrue(actualLabel);
     });
 
-    it('should verify label "Cell phone number" font-family.', () => {
+    it('should check label "Cell phone number" font-family.', () => {
         const actualLabelObj = $('//label[@for="phone"]').getCSSProperty('font-family');
         const valuesfontFamilyObj = Object.values(actualLabelObj)[2];
         const actualLabelfontFamily = Object.values(valuesfontFamilyObj).join();
@@ -111,28 +109,28 @@ describe('Cell phone number Label', () => {
         assert.equal(actualLabelfontFamily, expectedLabelfontFamily);
     });
 
-    it('should verify label "Cell phone number" font-size', () => {
+    it('should check label "Cell phone number" font-size', () => {
         const labelSizeObj = $('//label[@for="phone"]').getCSSProperty('font-size').value;
         console.log('!!!!!!!!!' + labelSizeObj);
         const expectedLabelSize = '17px';
         assert.equal(labelSizeObj, expectedLabelSize);
     });
 
-    it('should verify label "Cell phone number" font-color', () => {
+    it('should check label "Cell phone number" font-color', () => {
         const actualLabelColor = $('//label[@for="phone"]').getCSSProperty('color').parsed.hex;
         console.log(actualLabelColor);
         const expectLabelColor = '#212529';
         assert.equal(actualLabelColor, expectLabelColor);
     });
 
-    it('should verify label "Cell phone number" font-weight', () => {
+    it('should check label "Cell phone number" font-weight', () => {
         const actualLabelWeight = $('//label[@for="phone"]').getCSSProperty('font-weight').value;
         console.log('!!!!!!' + actualLabelWeight);
         const expectLabelWeight = 400;
         assert.equal(actualLabelWeight, expectLabelWeight);
     });
 
-    it('should verify label "Cell phone number" text-align', () => {
+    it('should check label "Cell phone number" text-align', () => {
         const actualLabelAlign = $('//label[@for="phone"]').getCSSProperty('text-align').value;
         console.log('!!!!!!' + actualLabelAlign);
         const expectLabelAlign = 'left';
@@ -145,47 +143,47 @@ describe('Cell phone number description text', () => {
         browser.url(url.registerUrl);
     });
 
-    it('should verify that label “Cell phone number”  is displayed.', () => {
+    it('should check that label “Cell phone number”  is displayed.', () => {
         const actualDescText = $('//input[@name="phone"]/following-sibling::small').isDisplayed();
         assert.isTrue(actualDescText);
     });
 
-    it('should verify that description text is “Format +17770005511 or +380653332244”', () => {
+    it('should check that description text is “Format +17770005511 or +380653332244”', () => {
         const actualDescText = $('//input[@name="phone"]/following-sibling::small').getText();
         console.log('!!!!!!!!' + actualDescText);
         const expectedDescText = 'Format +17770005511 or +380653332244';
         assert.equal(actualDescText, expectedDescText);
     });
 
-    it('should verify description text font-family', () => {
+    it('should check description text font-family', () => {
         const actualDescTextFontFamily = $('//input[@name="phone"]/following-sibling::small').getCSSProperty('font-family').parsed.value.join();
         console.log('!!!!!!!!' + actualDescTextFontFamily);
         const expectedDescTextFontFamily = 'sf pro display,sf pro icons,helvetica neue,helvetica,arial,sans-serif';
         assert.equal(actualDescTextFontFamily, expectedDescTextFontFamily);
     });
 
-    it('should verify description text font-size', () => {
+    it('should check description text font-size', () => {
         const actualDescTextSize = $('//input[@name="phone"]/following-sibling::small').getCSSProperty('font-size').value;
         console.log('!!!!!!!!' + actualDescTextSize);
         const expectedDescTextSize = '13.6px';
         assert.equal(actualDescTextSize, expectedDescTextSize);
     });
 
-    it('should verify description text font-color', () => {
+    it('should check description text font-color', () => {
         const actualDescTextColor = $('//input[@name="phone"]/following-sibling::small').getCSSProperty('color').parsed.hex;
         console.log('!!!!!!!!' + actualDescTextColor);
         const expectedDescTextColor = '#6c757d';
         assert.equal(actualDescTextColor, expectedDescTextColor);
     });
 
-    it('should verify description text font-weight', () => {
+    it('should check description text font-weight', () => {
         const actualDescTextWeight = $('//input[@name="phone"]/following-sibling::small').getCSSProperty('font-weight').value;
         console.log('!!!!!!!!' + actualDescTextWeight);
         const expectedDescTextWeight = 400;
         assert.equal(actualDescTextWeight, expectedDescTextWeight);
     });
-    
-    it('should verify description text text-align', () => {
+
+    it('should check description text text-align', () => {
         const actualDescTextAlign = $('//input[@name="phone"]/following-sibling::small').getCSSProperty('text-align').value;
         console.log('!!!!!!!!' + actualDescTextAlign);
         const expectedDescTextAlign = 'left';
